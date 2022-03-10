514c603926cba3f43d370fa1fe5d24b2583ddbe6
natnatjr <ejnatnat@yahoo.com>
2022-03-10 15:34:12 -0500

    Added a class "PasswordValidator" that extends UnoOnline so that password validation rules can be changed outside of main class. Added a comment.

--- src/midtermreviewcodeforpartc/PasswordValidator.java
+++ src/midtermreviewcodeforpartc/PasswordValidator.java
@@ -2,6 +2,8 @@ package midtermreviewcodeforpartc;
 /**
  * @author Rich Smith at ZenOfProgramming.com
  */
+
+// this class can be used to modify UnoOnline password validation indirectly
 public class PasswordValidator extends UnoOnline
 {
 
